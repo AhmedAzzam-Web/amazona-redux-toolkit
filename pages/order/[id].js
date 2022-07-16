156496f1e330854f5d192c11f2b1d6699dbbe629
Ahmed azzam <ahmedazzam.business@gmail.com>
2022-07-16 03:31:55 +0200

    created back-end ([id] folder index.js file) api to return order information based on id

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,19 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import dynamic from 'next/dynamic'
 import { Layout, Loading } from '../../components/imports'
 import { Card, CardContent, Grid, Link, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 import { useSelector, useDispatch } from 'react-redux'
 import NextLink from "next/link";
 import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { useSnackbar } from 'notistack'
+import { getError } from '../../utils/error'
+import axios from 'axios'
 
 const Order = ({ params }) => {
   const { id: orderId } = params;
+  const router = useRouter()
   const [loading, setLoading] = useState(false)
   const { orderDetails } = useSelector((store) => store.order)
+  const { userData } = useSelector((store) => store.user)
+  const { enqueueSnackbar } = useSnackbar();
 
-  console.log(orderDetails);
+  const { shippingData, cartItems, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice } = orderDetails;
 
-  const { shippingData, cartItems, paymentMethod } = orderDetails;
+  useEffect(() => {
+    if (!userData) {
+      return router.push('/login')
+    }
+
+    const fetchOrder = async () => {
+      try {
+        setLoading(true)
+        const { data } = axios.get(`/api/orders/${orderId}`, {
+          headers: {
+            authorization: `Bearer ${userData.token}`
+          }
+        })
+        setLoading(false)
+      } catch (error) {
+        setLoading(true)
+        enqueueSnackbar(getError(error), { variant: 'error' })
+      }
+    }
+
+    fetchOrder()
+
+    return () => { }
+  }, [orderId, userData])
 
   return (
     <Layout title={`Order ${orderId}`}>
@@ -115,6 +145,72 @@ const Order = ({ params }) => {
                   </TableContainer>
                 </CardContent>
 
+              </Card>
+            </Grid>
+
+            <Grid item xs={12} md={3}>
+              <Card className="card">
+                <CardContent>
+                  <Typography variant="h2" component="h2">
+                    Order Summary
+                  </Typography>
+
+                  <Grid container sx={{ marginTop: '15px' }}>
+                    <Grid item xs={6}>
+                      <Typography variant="body1" component="h4">
+                        Items:
+                      </Typography>
+                    </Grid>
+
+                    <Grid item xs={6}>
+                      <Typography variant="body1" component="h4" align='right'>
+                        ${itemsPrice}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+
+                  <Grid container sx={{ marginTop: '15px' }}>
+                    <Grid item xs={6}>
+                      <Typography variant="body1" component="h4">
+                        Tax:
+                      </Typography>
+                    </Grid>
+
+                    <Grid item xs={6}>
+                      <Typography variant="body1" component="h4" align='right'>
+                        ${taxPrice}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+
+                  <Grid container sx={{ marginTop: '15px' }}>
+                    <Grid item xs={6}>
+                      <Typography variant="body1" component="h4">
+                        Shipping:
+                      </Typography>
+                    </Grid>
+
+                    <Grid item xs={6}>
+                      <Typography variant="body1" component="h4" align='right'>
+                        ${shippingPrice}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+
+                  <Grid container sx={{ marginTop: '15px' }}>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle1" component="h4" sx={{ fontWeight: 'bold' }}>
+                        Total:
+                      </Typography>
+                    </Grid>
+
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle1" component="h4" align='right' sx={{ fontWeight: 'bold' }}>
+                        ${totalPrice}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </CardContent>
               </Card>
             </Grid>
           </Grid>
